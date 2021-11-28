93cc4160f84f2683f3150b363950c24afa5295ef
Nahuel Agustin Chaikh <nahuel.chaikh@gmail.com>
2021-11-28 20:04:52 -0300

    .

--- querys.ts
+++ querys.ts
@@ -14,7 +14,7 @@ const Querys = {
 
     putEntradaPreshow: (idEntradas, presid) => `UPDATE Entradas  SET Preshow=GETDATE(), PreSid='${presid}' WHERE idEntradas=${idEntradas}`,
 
-    postCliente: (data) => `INSERT INTO DevicesBackup (espectaculo_id, dni, fechayhora, personas, sala) VALUES ('${data.espectaculo_id}', '${data.dni}', '${data.fechayhora}', '${data.personas}', '${data.sala}')`
+    postCliente: (data) => `INSERT INTO DevicesBackup (espectaculo_id, dni, fechayhora, personas, sala, device) VALUES ('${data.espectaculo_id}', '${data.dni}', '${data.fechayhora}', '${data.personas}', '${data.sala}', '${data.device}')`
 
 }
 
